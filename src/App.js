5f1615fc77b517aab5f9150925bc1a539771584f
Katoria <Pushpendra.Katoria@Dell.com>
2022-08-29 16:34:53 +0530

    doubeounce logic basic example

--- src/App.js
+++ src/App.js
@@ -3,15 +3,13 @@ import cities from './cities.json'
 import './App.css';
 import CityFilter from './CityFilter';
 import CityFilterDebounce from './CityFilterDebounce';
-import StopWatch from './StopWatch';
 
 function App() {
   return (
     <div className="App">
       <h1>Debouncing example</h1>
-      {/* <CityFilter cities={cities}/> */}
+      <CityFilter cities={cities}/>
       {/* <CityFilterDebounce cities={cities}/> */}
-      <StopWatch/>
     </div>
   );
 }
